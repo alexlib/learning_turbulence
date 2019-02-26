69afaf197e52007ad505de8388b7edf560a27eb2
Keaton J. Burns <keaton.burns@gmail.com>
2019-02-26 17:15:43 -0500

    Flake and add some comments

--- scripts/nets/train_unet.py
+++ scripts/nets/train_unet.py
@@ -1,69 +1,43 @@
+"""Train u-net."""
 
-
-import tensorflow as tf
 import numpy as np
-
-
-
+import tensorflow as tf
+from .unet import Unet
 tf.enable_eager_execution()
 
-execfile("Unet.py")
-
-
-
+# Parameters
 RESTORE = False
 train_batch_size = 1
 num_features = 128
-
-
-Unet_obj = Unet()
-
-
-
-
-
-
-
-optimizer = tf.train.AdamOptimizer(1.0e-3)
 checkpoint_path = "checkpoints/model_Unet"
+datatype = tf.float32
 
-if (RESTORE==True):
-	Unet_obj.load_weights(checkpoint_path)
+# Build network and optimizer
+Unet_obj = Unet()
+optimizer = tf.train.AdamOptimizer(1.0e-3)
+if RESTORE:
+    Unet_obj.load_weights(checkpoint_path)
 
+# Allocate space for input and output
+labels = np.zeros((train_batch_size, num_features, num_features, 1))
+Net_input = np.zeros((train_batch_size, num_features, num_features, 1))
 
-labels = np.zeros((train_batch_size,num_features, num_features,1))
-
-Net_input = np.zeros((train_batch_size,num_features, num_features,1))
-
-
+# Learning loop
 with tf.device('/gpu:0'):
     for epoch in range(1):
         for train_iter in range(1):
-
-            #loads labels and network inputs here and asign to labels and Net_input
-
-            tf_labels = tf.cast( labels, datatype)
-
-
-            tf_Net_input = tf.cast( Net_input , datatype)
-
-
-
+            # Loads labels and network inputs here and asign to labels and Net_input
+            tf_labels = tf.cast(labels, datatype)
+            tf_Net_input = tf.cast(Net_input, datatype)
+            # Optimization step
             with tf.GradientTape() as tape:
                 tape.watch(Unet_obj.variables)
-                cost_value , alpha_net = Unet_obj.cost_function(tf_Net_input , tf_labels )
-            weight_grads = tape.gradient(cost_value, [Unet_obj.variables] )
-
+                cost_value, alpha_net = Unet_obj.cost_function(tf_Net_input, tf_labels)
+            weight_grads = tape.gradient(cost_value, [Unet_obj.variables])
             #clipped_grads = [tf.clip_by_value(grads_i,-10,10) for grads_i in weight_grads[0]]
             optimizer.apply_gradients(zip(weight_grads, Unet_obj.variables), global_step=tf.train.get_or_create_global_step())
-
-            print( epoch, train_iter , cost_value.numpy() )
-
-            if (((train_iter+1)%10)==0):
-                print( "saving weights." )
+            # Status and output
+            print(epoch, train_iter, cost_value.numpy())
+            if ((train_iter+1) % 10) == 0:
+                print("saving weights.")
                 RT.save_weights("checkpoints/model_Unet")
-
-
-
-
-
--- scripts/nets/unet.py
+++ scripts/nets/unet.py
@@ -1,11 +1,11 @@
+"""Define u-net."""
+
+
 from tensorflow.python.keras.layers.merge import concatenate
 
 
-
-
-
-datatype = tf.float32
 class Unet(tf.keras.Model):
+
     def __init__(self):
         super(Unet, self).__init__()
 
@@ -55,60 +55,58 @@ class Unet(tf.keras.Model):
         self.Loutputs = tf.keras.layers.Conv2D(2, (1, 1), activation='linear')
 
     def call(self, in_Net):
-        c1 = self.Lc11 (in_Net)
-        c1 = self.Lc12 (c1)
-        p1 = self.Lp13 (c1)
+        c1 = self.Lc11(in_Net)
+        c1 = self.Lc12(c1)
+        p1 = self.Lp13(c1)
 
-        c2 = self.Lc21 (p1)
-        c2 = self.Lc22 (c2)
-        p2 = self.Lp23 (c2)
+        c2 = self.Lc21(p1)
+        c2 = self.Lc22(c2)
+        p2 = self.Lp23(c2)
 
-        c3 = self.Lc31 (p2)
-        c3 = self.Lc32 (c3)
-        p3 = self.Lp33 (c3)
+        c3 = self.Lc31(p2)
+        c3 = self.Lc32(c3)
+        p3 = self.Lp33(c3)
 
-        c4 = self.Lc41 (p3)
-        c4 = self.Lc42 (c4)
-        p4 = self.Lp43 (c4)
+        c4 = self.Lc41(p3)
+        c4 = self.Lc42(c4)
+        p4 = self.Lp43(c4)
 
-        c5 = self.Lc51 (p4)
-        c5 = self.Lc52 (c5)
-        p5 = self.Lp53 (c5)
+        c5 = self.Lc51(p4)
+        c5 = self.Lc52(c5)
+        p5 = self.Lp53(c5)
 
-        z1 = self.LcZ1 (p5)
-        z1 = self.LcZ2 (z1)
+        z1 = self.LcZ1(p5)
+        z1 = self.LcZ2(z1)
 
-        u6 = self.Lu61 (z1)
+        u6 = self.Lu61(z1)
         u6 = tf.concat([u6, c5], axis=3)
-        c6 = self.Lc62 (u6)
-        c6 = self.Lc63 (c6)
+        c6 = self.Lc62(u6)
+        c6 = self.Lc63(c6)
 
-        u7 = self.Lu71 (c6)
+        u7 = self.Lu71(c6)
         u7 = tf.concat([u7, c4], axis=3)
-        c7 = self.Lc72 (u7)
-        c7 = self.Lc73 (c7)
+        c7 = self.Lc72(u7)
+        c7 = self.Lc73(c7)
 
-        u8 = self.Lu81 (c7)
+        u8 = self.Lu81(c7)
         u8 = tf.concat([u8, c3], axis=3)
-        c8 = self.Lc82 (u8)
-        c8 = self.Lc83 (c8)
+        c8 = self.Lc82(u8)
+        c8 = self.Lc83(c8)
 
-        u9 = self.Lu91 (c8)
+        u9 = self.Lu91(c8)
         u9 = tf.concat([u9, c2], axis=3)
-        c9 = self.Lc92 (u9)
-        c9 = self.Lc93 (c9)
+        c9 = self.Lc92(u9)
+        c9 = self.Lc93(c9)
 
-        u10 = self.Lu101 (c9)
+        u10 = self.Lu101(c9)
         u10 = tf.concat([u10, c1], axis=3)
-        c10 = self.Lc102 (u10)
-        c10 = self.Lc103 (c10)
+        c10 = self.Lc102(u10)
+        c10 = self.Lc103(c10)
 
-        outputs = self.Loutputs (c10)
-
+        outputs = self.Loutputs(c10)
         return outputs
 
-    def cost_function( self, in_Net , labels):
+    def cost_function(self, in_Net, labels):
         alpha = self.call(in_Net)
-
-        return tf.reduce_mean( ( alpha - labels)**2 ) , alpha
+        return tf.reduce_mean((alpha - labels)**2), alpha
 
